70d13675b66a305f98afdd861976a89507a4eef5
Crowdbotics <team@crowdbotics.com>
2020-09-25 19:56:28 +0000

    add package blueprint-react-native-sign-in-4-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import SignIn43110389Navigator from '../features/SignIn43110389/navigator';
 import Dashboard13110388Navigator from '../features/Dashboard13110388/navigator';
 import BlankScreen0110387Navigator from '../features/BlankScreen0110387/navigator';
 
@@ -15,6 +16,7 @@ import BlankScreen0110387Navigator from '../features/BlankScreen0110387/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+SignIn43110389: { screen: SignIn43110389Navigator },
 Dashboard13110388: { screen: Dashboard13110388Navigator },
 BlankScreen0110387: { screen: BlankScreen0110387Navigator },
 
--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import SignIn43110389Reducer from '../features/SignIn43110389/redux/reducers'
 import Dashboard13110388Reducer from '../features/Dashboard13110388/redux/reducers'
 
 export const combinedReducers = combineReducers({
@@ -16,6 +17,7 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+SignIn43110389: SignIn43110389Reducer,
 Dashboard13110388: Dashboard13110388Reducer,
 
 });
